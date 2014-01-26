b02d0dbe6ae03642d62240b8db235e17415fbaa2
Bernhard Schwarz <bschwrz@gmail.com>
2014-01-26 20:35:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+goldenbot-commands
+==================
+
+goldenbot-commands makes it easy to add additional commands to your bot. You might want to use it to display the next map or the current time on demand.
---
+++ cod/cod_commands.go
@@ -0,0 +1,45 @@
+package cod
+
+import (
+	"errors"
+	"github.com/adabei/goldenbot/events"
+	rcon "github.com/adabei/goldenbot/rcon/cod"
+	"github.com/adabei/goldenbut/events/cod"
+)
+
+type Commands struct {
+	commands map[string]func()
+	events   chan interface{}
+	requests chan rcon.RCONQuery
+}
+
+func NewCommands(requests chan rcon.RCONQuery, ea events.Aggregator) *Commands {
+	c := new(Commands)
+	c.requests = requests
+	c.events = ea.Subscribe(c)
+	return c
+}
+
+func (c *Commands) Setup() error {
+	return nil
+}
+
+func (c *Commands) Start() {
+	for {
+		ev := <-c.events
+		if ev, ok := in.(cod.Say); ok {
+			if cmd, ok := c.commands[ev.Message]; ok {
+				cmd()
+			}
+		}
+	}
+}
+
+func (c *Commands) Register(cmd string, fn func()) error {
+	if _, dup := c.commands[cmd]; dup {
+		return errors.New("Command %v already defined", cmd)
+	}
+
+	c.commands[cmd] = fn
+	return nil
+}
